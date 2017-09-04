8c14f980b4ca1fe49b1e5eac247d0404a03a95dd
TuanBui <buiductuan811@gmail.com>
2017-09-04 16:06:27 +0700

    update api config

--- BookServicesNet/App_Start/WebApiConfig.cs
+++ BookServicesNet/App_Start/WebApiConfig.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-
+using System.Net.Http.Headers;
 namespace BookServicesNet
 {
     public static class WebApiConfig
@@ -19,6 +19,8 @@ namespace BookServicesNet
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
